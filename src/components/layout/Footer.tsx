59dd7ee00e3bc24ec57fe3c2120f56e7ac6ea362
hyerini <rinlee2001@naver.com>
2024-11-18 17:59:08 +0900

    style(layout): footer 텍스트 색상 변경

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 // Footer.tsx
 export const Footer = () => {
     return (
-        <footer className="bg-white h-20 flex items-center justify-center p-4 mt-auto">
+        <footer className="bg-white text-gray-500 h-20 flex items-center justify-center p-4 mt-auto">
             @CopyRight Boongeo
         </footer>
     );
