4ec23ce8c627b1ae1fc5a4de1b8f3e25cb630b6a
yooju00 <yooju00@naver.com>
2024-11-19 02:00:29 +0900

    style(config): SideBarItem 오타 수정

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -1,9 +1,34 @@
-import { SideVarItem } from "@/types/sidebar";
+import { SideBarItem } from "@/types/sidebar";
 
-export const sideItems: SideVarItem[] = [
-    { id: "home", name: "Home", href: "#", icon: "/images/Home.png" },
-    { id: "message", name: "Message", href: "#", icon: "/images/Message.png" },
-    { id: "explore", name: "Explore", href: "#", icon: "/images/Explore.png" },
-    { id: "booking", name: "Booking", href: "#", icon: "/images/Booking.png" },
-    { id: "mypage", name: "MyPage", href: "#", icon: "/images/Mypage.png" },
+export const sideItems: SideBarItem[] = [
+    {
+        id: "home",
+        name: "Home",
+        href: "#",
+        icon: "/images/Home.png"
+    },
+    {
+        id: "message",
+        name: "Message",
+        href: "#",
+        icon: "/images/Message.png"
+    },
+    {
+        id: "explore",
+        name: "Explore",
+        href: "#",
+        icon: "/images/Explore.png"
+    },
+    {
+        id: "booking",
+        name: "Booking",
+        href: "#",
+        icon: "/images/Booking.png"
+    },
+    {
+        id: "mypage",
+        name: "MyPage",
+        href: "#",
+        icon: "/images/Mypage.png"
+    },
 ];
