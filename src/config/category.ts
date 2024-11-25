5e97628a70987d759f4901042c6f6a0ebebe1f12
hyerini <rinlee2001@naver.com>
2024-11-25 19:03:08 +0900

    feat(config): ReviewCategory 상수 추가

--- src/config/category.ts
+++ src/config/category.ts
@@ -5,3 +5,12 @@ export const Category = [
     { value: "devOps", label: "DevOps" },
     { value: "ui/ux", label: "UI/UX" }
 ];
+
+export const ReviewCategories = [
+    "Technically competent",
+    "Very motivational",
+    "Amazing communicator",
+    "Amazing problem solver",
+    "Empathetic listener",
+    "Encouraging mentor",
+];
