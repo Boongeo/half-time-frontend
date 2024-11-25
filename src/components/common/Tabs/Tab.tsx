f80446ae3a44c67e26858b7d22b7a012d156e33b
hyerini <rinlee2001@naver.com>
2024-11-25 19:00:52 +0900

    feat(tabs): Tab 컴포넌트 구현

---
+++ src/components/common/Tabs/Tab.tsx
@@ -0,0 +1,28 @@
+import { forwardRef } from "react";
+import clsx from "clsx";
+import { TabProps } from "@/types/commonProps";
+
+const Tab = forwardRef<HTMLDivElement, TabProps>(
+    ({ label, value, selected, onTabSelect, className, ...props }, ref) => {
+        return (
+            <div
+                ref={ref}
+                onClick={() => onTabSelect(value)} // 클릭 시 탭 변경
+                className={clsx(
+                    "cursor-pointer text-lg tracking-wider pb-2",
+                    selected
+                        ? "text-themeColor border-b-4 border-themeColor"
+                        : "text-gray-600 hover:text-themeColor",
+                    className
+                )}
+                {...props}
+            >
+                {label}
+            </div>
+        );
+    }
+);
+
+Tab.displayName = "Tab";
+
+export default Tab;
---
+++ src/components/common/Tabs/Tabs.tsx
@@ -0,0 +1,25 @@
+import { forwardRef } from "react";
+import Tab from "./Tab";
+import { TabsProps } from "@/types/commonProps";
+
+const Tabs = forwardRef<HTMLDivElement, TabsProps>(
+    ({ tabs, selectedTab, onTabSelect }, ref) => {
+        return (
+            <div ref={ref} className="flex gap-8 w-full max-w-4xl justify-start">
+                {tabs.map((tab) => (
+                    <Tab
+                        key={tab.value}
+                        label={tab.label}
+                        value={tab.value}
+                        selected={selectedTab === tab.value}
+                        onTabSelect={onTabSelect}
+                    />
+                ))}
+            </div>
+        );
+    }
+);
+
+Tabs.displayName = "Tabs";
+
+export default Tabs;
