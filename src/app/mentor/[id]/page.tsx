3c9a536715da1ef2c8d1f708a83cb865239597c3
yooju00 <yooju00@naver.com>
2024-12-01 18:27:33 +0900

    fix(mentor): Next.js params 처리 관련 경고 해결

--- src/app/mentor/[id]/page.tsx
+++ src/app/mentor/[id]/page.tsx
@@ -1,18 +1,24 @@
-import { mockMentors } from "@/lib/mocks/mentors";
 import MentorDetailClient from "@/components/mentor/MentorDetailClient";
 import { notFound } from "next/navigation";
+import {MentorService} from "@/lib/services/mentorService";
 
-export default function MentorDetailPage({ params }: { params: { id: string } }) {
-    const mentorId = parseInt(params.id, 10);
-    const mentor = mockMentors.find((mentor) => mentor.id === mentorId);
+export default async function MentorDetailPage(props: { params: { id: string } }) {
+    const params = await props.params;
+    const mentorService = new MentorService();
+    const mentorId = Number(params.id);
 
-    if (!mentor) {
-        return notFound(); // 404 페이지 렌더링
+    try {
+        const response = await mentorService.getMentorById(mentorId);
+        if (!response.success || !response.data) {
+            return notFound();
+        }
+
+        return (
+            <div>
+                <MentorDetailClient mentor={response.data} />
+            </div>
+        )
+    } catch {
+        return notFound();
     }
-
-    return (
-        <div>
-            <MentorDetailClient mentor={mentor} />
-        </div>
-    );
 }
