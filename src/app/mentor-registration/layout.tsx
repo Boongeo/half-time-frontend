8fefe54359cf6f9193b6ae4295814be89f210dd6
yooju00 <yooju00@naver.com>
2024-12-04 18:48:36 +0900

    feat(mentor-registration): 멘토 등록 페이지 레이아웃 구현

---
+++ src/app/mentor-registration/layout.tsx
@@ -0,0 +1,14 @@
+import {Footer} from "@/components/layout/Footer";
+import {BaseHeader} from "@/components/layout/BaseHeader";
+
+export default function MentorRegistrationLayout({ children, }: { children: React.ReactNode; }) {
+    return (
+        <div className="flex min-h-screen flex-col bg-white">
+            <BaseHeader />
+            <main className="flex-1 container mx-auto px-4 py-8">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    );
+}
