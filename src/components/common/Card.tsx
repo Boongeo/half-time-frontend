df103be4169dcedcbcd1912c0a4f6ec7201b7ec5
yooju00 <yooju00@naver.com>
2025-02-13 19:07:19 +0900

    fix(common): 필요없는 props 제거

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -23,26 +23,11 @@ export const Card = forwardRef<HTMLDivElement, BoxProps>(({
                 <h2 className="text-2xl font-bold mb-4">{mainTitle}</h2>
             )}
 
-            {hasLogo ? (
-                <div className="flex flex-col justify-items-center">
-                    <div className="flex items-center mb-7">
-                        <Image
-                            src="/images/BookingListImg.png"
-                            alt="Booking list thumbnail"
-                            fill
-                            className="rounded object-cover"
-                            sizes="48px"
-                        />
-                        <div>
-                            {title && <h3 className="text-lg font-bold">{title}</h3>}
-                            {subtitle && <p className="text-sm text-gray-500">{subtitle}</p>}
-                        </div>
-                    </div>
-                    {children}
-                </div>
-            ) : (
-                <div>{children}</div>
-            )}
+            <div>
+                {title && <h3 className="text-lg font-bold">{title}</h3>}
+                {subtitle && <p className="text-sm text-gray-500">{subtitle}</p>}
+                {children}
+            </div>
         </div>
     );
     }
